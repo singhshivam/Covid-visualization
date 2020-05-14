11b5145ffa95a3a42a103ab37a55847c092d5dad
singhshivam <singhshivam.172@gmail.com>
2020-05-14 12:02:44 -0500

    restuctured Dashboard. cleaned routes.js

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,26 +5,7 @@ window.jQuery = $;
 window.$ = $;
 global.jQuery = $;
 
-const DashboardDefault = React.lazy(() => import('./Demo/Dashboard/Default'));
-
-const UIBasicButton = React.lazy(() => import('./Demo/UIElements/Basic/Button'));
-const UIBasicBadges = React.lazy(() => import('./Demo/UIElements/Basic/Badges'));
-const UIBasicBreadcrumbPagination = React.lazy(() => import('./Demo/UIElements/Basic/BreadcrumbPagination'));
-
-const UIBasicCollapse = React.lazy(() => import('./Demo/UIElements/Basic/Collapse'));
-const UIBasicTabsPills = React.lazy(() => import('./Demo/UIElements/Basic/TabsPills'));
-const UIBasicBasicTypography = React.lazy(() => import('./Demo/UIElements/Basic/Typography'));
-
-const FormsElements = React.lazy(() => import('./Demo/Forms/FormsElements'));
-
-const BootstrapTable = React.lazy(() => import('./Demo/Tables/BootstrapTable'));
-
-const Nvd3Chart = React.lazy(() => import('./Demo/Charts/Nvd3Chart/index'));
-
-const GoogleMap = React.lazy(() => import('./Demo/Maps/GoogleMap/index'));
-
-const OtherSamplePage = React.lazy(() => import('./Demo/Other/SamplePage'));
-const OtherDocs = React.lazy(() => import('./Demo/Other/Docs'));
+const DashboardDefault = React.lazy(() => import('./Dashboard/Default'));
 
 const routes = [
     { path: '/dashboard/default', exact: true, name: 'Covid-19 Home', component: DashboardDefault },
