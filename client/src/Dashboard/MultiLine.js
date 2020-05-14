c4a268a1bd67cc10b265f262aee8a6201e2c7eb8
singhshivam <singhshivam.172@gmail.com>
2020-05-14 13:40:03 -0500

    adding basic multiline graph

--- client/src/Dashboard/MultiLine.js
+++ client/src/Dashboard/MultiLine.js
@@ -1,14 +1,139 @@
 import React from 'react';
-import {Row, Col, Card, Table, Tabs, Tab} from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
+
+import * as d3 from 'd3'
 
 import Aux from "../hoc/_Aux";
-import DEMO from "../store/constant";
-
-import avatar1 from '../assets/images/user/avatar-1.jpg';
-import avatar2 from '../assets/images/user/avatar-2.jpg';
-import avatar3 from '../assets/images/user/avatar-3.jpg';
 
 class MultiLine extends React.Component {
+
+    componentDidMount() {
+        this.fetchCSVdata()
+    }
+
+    fetchCSVdata() {
+        let margin = { top: 10, right: 30, bottom: 30, left: 60 },
+            width = 1000 - margin.left - margin.right,
+            height = 600 - margin.top - margin.bottom;
+
+        let svg = d3.select(".g4-confirmed-deaths")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.csv("../../data/g4.csv")
+            .then((data)  => {
+                let covid = d3.nest()
+                    .key(function (d) { return d.Entity; })
+                    .entries(data);
+
+                let x = d3.scaleLinear()
+                    .domain([0, d3.max(data, function (d) { return +d.fivedeath; })])
+                    .range([0, width]);
+
+                svg.append("g")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(d3.axisBottom(x).ticks(5));
+                svg.append("text")
+                    .attr("transform",
+                        "translate(" + (width / 2) + " ," +
+                        (height + margin.top + 20) + ")")
+                    .style("text-anchor", "middle")
+                    .text("Days since 5 daily deaths first reported");
+                function make_x_gridlines() {
+                    return d3.axisBottom(x)
+                        .ticks(5)
+                } svg.append("g")
+                    .attr("class", "grid")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(make_x_gridlines()
+                        .tickSize(-height)
+                        .tickFormat("")
+                    )
+                let y = d3.scaleSymlog()
+                    .domain([0, 1e4])
+                    .range([height, 0]);
+                svg.append("g")
+                    .call(d3.axisLeft(y).ticks(5, "~s"));
+
+                svg.append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 0 - margin.left)
+                    .attr("x", 0 - (height / 2))
+                    .attr("dy", "1em")
+                    .style("text-anchor", "middle")
+                    .text("LOG");
+
+                svg.append("g")
+                    .attr("class", "grid")
+                    .call(make_y_gridlines()
+                        .tickSize(-width)
+                        .tickFormat("")
+                    )
+                function make_y_gridlines() {
+                    return d3.axisLeft(y)
+                        .ticks(5)
+                }
+                let res = covid.map(d => d.key)
+                let color = d3.scaleOrdinal(d3.schemeCategory10)
+                    .domain(res);
+                let div = d3.select(".graph-body").append("div")
+                    .attr("class", "tooltip-line")
+                    .style("opacity", 0);
+                const lines = svg.selectAll(".line")
+                    .data(covid)
+                    .enter()
+                    .append("path")
+                    .attr("fill", "none")
+                    .attr("stroke", function (d) { return color(d.key) })
+                    .attr("stroke-width", 1.5)
+                    .attr("d", function (d) {
+                        return d3.line()
+                            .x(function (d) { return x(d.fivedeath); })
+                            .y(function (d) { return y(d.deaths); })
+                            (d.values)
+                    })
+
+                    .on('mouseover', function (d, i) {
+                        console.log('d', d)
+                        console.log('i', i)
+                        const selection = d3.select(this).raise();
+                        selection
+                            .transition()
+                            .duration("10")
+                            .attr("stroke", function (d) { return color(d.key) })
+                            .attr("opacity", "0.85")
+                            .attr("stroke-width", 3.5)
+                        div.transition()
+                            .duration(50)
+                            .style("opacity", 1);
+
+                        if (d.values[i]) {
+                            div.html(d.key + "</br>" + d.values[i].deaths + " deaths")
+                                .style("left", (d3.event.pageX + 10) + "px")
+                                .style("top", (d3.event.pageY - 15) + "px");
+                        }
+                    })
+
+                    .on('mouseout', function (d, i) {
+                        const selection = d3.select(this)
+                        selection
+                            .transition()
+                            .duration("10")
+                            .attr("stroke", function (d) { return color(d.key) })
+                            .attr("opacity", "1")
+                            .attr("stroke-width", 1.5);
+                        div.transition()
+                            .duration('50')
+                            .style("opacity", 0);
+
+                    });
+
+            })
+    }
     render() {
         return (
             <Aux>
@@ -17,17 +142,9 @@ class MultiLine extends React.Component {
                         <Card>
                             <Card.Body>
                                 <h6 className='mb-4'>Daily Sales</h6>
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-9">
-                                        <h3 className="f-w-300 d-flex align-items-center m-b-0"><i className="feather icon-arrow-up text-c-green f-30 m-r-5"/> $249.95</h3>
+                                <div className="row d-flex align-items-center graph-body">
+                                    <div className="col-12 g4-confirmed-deaths">
                                     </div>
-
-                                    <div className="col-3 text-right">
-                                        <p className="m-b-0">50%</p>
-                                    </div>
-                                </div>
-                                <div className="progress m-t-30" style={{height: '7px'}}>
-                                    <div className="progress-bar progress-c-theme" role="progressbar" style={{width: '50%'}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"/>
                                 </div>
                             </Card.Body>
                         </Card>
