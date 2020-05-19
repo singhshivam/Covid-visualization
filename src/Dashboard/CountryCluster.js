eee39279ee5eafa64448b43615a390bf4eee9182
singhshivam <singhshivam.172@gmail.com>
2020-05-19 01:09:27 -0500

    axis labeling clustering data

--- src/Dashboard/CountryCluster.js
+++ src/Dashboard/CountryCluster.js
@@ -121,6 +121,25 @@ class CountryCluster extends React.Component {
                     .text("K-clusters:")
                     .style("text-anchor", "end")
                     .attr("transform", "translate(150,0)");
+                // text label for the x axis
+                svg.append("text")
+                    .attr("transform",
+                        "translate(" + (width / 2) + " ," +
+                        (height + margin.top + 20) + ")")
+                    .style("text-anchor", "middle")
+                    .text("Confirmed Cases");
+
+
+                // text label for the y axis
+                svg.append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 0 - margin.left)
+                    .attr("x", 0 - (height / 2))
+                    .attr("dy", "1em")
+                    .style("text-anchor", "middle")
+                    .text("Deaths");      
+
+
             })
     }
 
